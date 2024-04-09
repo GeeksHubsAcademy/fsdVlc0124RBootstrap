4264b635bd08f26f6e0ccbd19b8f82fb05593987
Dave86dev <davidochando1986@gmail.com>
2024-04-09 10:26:08 +0200

    feat: React bootstrap ejemplo básico

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import "bootstrap/dist/css/bootstrap.min.css";
+import { bringCharacters } from "./services/apiCalls";
+
+// import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from "react-bootstrap";
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    if (characters.length === 0) {
+      bringCharacters()
+        .then((fetched) => {
+          setCharacters(fetched.data.results);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [characters]);
+
+  return (
+    <>
+      {characters.length > 0 ? (
+        <Container fluid>
+          <Row className="center">
+            {characters.map((character) => {
+              return <Col sm={12} lg={6} key={character.id}>{character.name}</Col>;
+            })}
+          </Row>
+        </Container>
+      ) : (
+        <div>No tenemos nada</div>
+      )}
+    </>
+  );
+}
+
+export default App;
